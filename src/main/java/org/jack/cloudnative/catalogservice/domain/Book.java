f710d7483a8184ea8f4b26da6a54a2c067da1880
jack <jack.li.cn@outlook.com>
2024-11-10 16:44:52 +0800

    Added validation rules for Book

--- src/main/java/org/jack/cloudnative/catalogservice/domain/Book.java
+++ src/main/java/org/jack/cloudnative/catalogservice/domain/Book.java
@@ -1,11 +1,17 @@
 package org.jack.cloudnative.catalogservice.domain;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 
 public record Book(
         @NotBlank(message = "The book ISBN must be defined.")
+        @Pattern(regexp = "^([0-9]{10} | [0-9]{13})$", message = "The ISBN format must be valid.")
         String isbn,
         String title,
         String author,
+        @NotNull(message = "The book price must be defined.")
+        @Positive(message = "The book price must be greater than zero.")
         Double price) {
 }
